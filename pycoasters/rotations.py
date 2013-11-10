f95951d9e0e0e94de1f35fe3cf3df36b0f5a69e7
Robin Deits <robin.deits@gmail.com>
2013-11-10 16:55:20 -0500

    cleanup

--- pycoasters/rotations.py
+++ pycoasters/rotations.py
@@ -9,8 +9,8 @@ def quat2rotmat(q):
     q = q / norm(q)
     w,x,y,z = q[0], q[1], q[2], q[3]
     return np.array([[w*w + x*x - y*y - z*z, 2*x*y - 2*w*z, 2*x*z + 2*w*y],
-              [2*x*y + 2*w*z,  w*w + y*y - x*x - z*z, 2*y*z - 2*w*x],
-              [2*x*z - 2*w*y, 2*y*z + 2*w*x, w*w + z*z - x*x - y*y]])
+          [2*x*y + 2*w*z,  w*w + y*y - x*x - z*z, 2*y*z - 2*w*x],
+          [2*x*z - 2*w*y, 2*y*z + 2*w*x, w*w + z*z - x*x - y*y]])
 
 def axis2rotmat(axis):
     """
