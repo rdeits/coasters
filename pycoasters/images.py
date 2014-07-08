2d499be4f3e334b3656a0157ff8fa88626962441
Robin Deits <robin.deits@gmail.com>
2014-07-08 01:03:04 -0400

    clean up original post

--- pycoasters/images.py
+++ pycoasters/images.py
@@ -1,12 +1,12 @@
 import pylab as pl
 
 def show(img,savepath=None):
-    dpi = 72.0
-    fig = pl.figure(figsize=[x/dpi for x in img.shape[:2]], dpi=dpi)
-    pl.figure()
+    # dpi = 72.0
+    # fig = pl.figure(figsize=[x/dpi for x in img.shape[:2]], dpi=dpi)
+    f = pl.figure()
     pl.imshow(img)
     pl.gca().get_xaxis().set_visible(False)
     pl.gca().get_yaxis().set_visible(False)
     if savepath is not None:
         pl.savefig(savepath)
-    return fig
+    # return fig
